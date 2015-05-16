3ae2ebb3a5c22a2d4244ff1050b714265d7f1656
ZZY <909645105@qq.com>
2015-05-16 22:44:24 +0800

    Update TransitionFunc.h

--- TransitionFunc.h
+++ TransitionFunc.h
@@ -1,7 +1,14 @@
 #include "parser.h"
+#include <vector>
+using namespace std;
 
-extern SyntaxTree sTree;
-extern int numState;
 
-void FuncInit(char* src,int numstat);
+extern SyntaxTree *sTree;
+extern int numStates;
+extern int numNeighbors;
+extern int ruleTree[2048][32];
+extern int num_nodes;
+
+void FuncInit(char* src,int numstat,int numneigh);
 int Function(int nw,int n,int ne,int w,int c,int e,int sw,int s,int se);
+void GetRuleTree();
