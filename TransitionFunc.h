3c2dc6791b9c4f7087aa7046fc28b45e12138ec0
ZZY <909645105@qq.com>
2015-05-16 16:03:56 +0800

    Create TransitionFunc.h

---
+++ TransitionFunc.h
@@ -0,0 +1,7 @@
+#include "parser.h"
+
+extern SyntaxTree sTree;
+extern int numState;
+
+void FuncInit(char* src,int numstat);
+int Function(int nw,int n,int ne,int w,int c,int e,int sw,int s,int se);
