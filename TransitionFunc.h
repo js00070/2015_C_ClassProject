22bb78b7279d7bc5d6c365aa0e831f79a60647f8
ZZY <909645105@qq.com>
2015-05-17 19:36:26 +0800

    Update TransitionFunc.h

--- TransitionFunc.h
+++ TransitionFunc.h
@@ -12,3 +12,12 @@ extern int num_nodes;
 void FuncInit(char* src,int numstat,int numneigh);
 int Function(int nw,int n,int ne,int w,int c,int e,int sw,int s,int se);
 void GetRuleTree();
+
+inline int CheckRule(int* a)//nw, ne, sw, se, n, w, e, s, c
+{
+	int p = num_nodes - 1;
+	int end = numNeighbors + 1;
+	for (int i = 0; i < end; ++i)
+		p = ruleTree[p][a[i] + 1];
+	return p;
+}
