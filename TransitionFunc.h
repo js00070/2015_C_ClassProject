00976ec24fe88e9ed9ce7e175c7c4a34eb9354f6
ZZY <909645105@qq.com>
2015-05-19 13:21:58 +0800

    Update TransitionFunc.h

--- TransitionFunc.h
+++ TransitionFunc.h
@@ -1,23 +1,44 @@
 #include "parser.h"
-#include <vector>
+#include <map>
+#include <utility>
 using namespace std;
 
+typedef char _BYTE;
+struct Point;
+typedef pair<int, int> Coordinate;
 
 extern SyntaxTree *sTree;
 extern int numStates;
 extern int numNeighbors;
 extern int ruleTree[2048][32];
 extern int num_nodes;
+extern map < Coordinate , Point* > pSet;
 
 void FuncInit(char* src,int numstat,int numneigh);
 int Function(int nw,int n,int ne,int w,int c,int e,int sw,int s,int se);
 void GetRuleTree();
-
-inline int CheckRule(int* a)//nw, ne, sw, se, n, w, e, s, c
+inline short CheckRule(_BYTE* a)//[nw, ne, sw, se,] n, w, e, s, c
 {
-	int p = num_nodes - 1;
-	int end = numNeighbors + 1;
-	for (int i = 0; i < end; ++i)
+	short i;
+	short p = 0;
+	short end = numNeighbors + 1;
+	for (i = 0; i < end; ++i)
+		p=p||a[i];
+	if (!p)
+		return -1; //四周邻居以及自己的状态都为NULL
+	p = num_nodes - 1;
+	for (i = 0; i < end; ++i)
 		p = ruleTree[p][a[i] + 1];
 	return p;
 }
+
+struct Point
+{
+	Coordinate pos;
+	_BYTE state;
+	Point* neighbors[8];//[nw, ne, sw, se,] n, w, e, s
+	map<Coordinate, Point*>::iterator it;
+	Point(int x,int y,_BYTE in);
+	~Point();
+	void Update();
+};
