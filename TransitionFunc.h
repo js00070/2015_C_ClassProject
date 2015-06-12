6fbbb28b3b7711ae6a6c875fff1d09f5ed14ae65
ZZY <909645105@qq.com>
2015-06-12 12:14:54 +0800

    Update TransitionFunc.h

--- TransitionFunc.h
+++ TransitionFunc.h
@@ -1,6 +1,7 @@
 #include "parser.h"
 #include <map>
 #include <utility>
+#include <fstream>
 using namespace std;
 
 typedef char _BYTE;
@@ -15,6 +16,7 @@ extern int num_nodes;
 extern map < Coordinate , Point* > pSet;
 extern int tmpDx[3][8];
 extern int tmpDy[3][8];
+extern int pointCount;
 
 struct Point
 {
@@ -25,20 +27,42 @@ struct Point
 	map<Coordinate, Point*>::iterator it;
 	Point(int x, int y, _BYTE in);
 	~Point();
+
 	void Update();
 };
 
+void NeighborsInit();
 void FuncInit(char* src,int numstat,int numneigh);
 int Function(int nw,int n,int ne,int w,int c,int e,int sw,int s,int se);
 void GetRuleTree();
 void AddPoint(int x, int y, _BYTE in);
 void DeletePoint(int x, int y, _BYTE in);
+inline int GetPointCount()
+{
+	return pointCount;
+}
+
+void ConvertWcharToChar(char*& dst,const wchar_t* src);
+
+void SaveMap(const wchar_t* fileName);
+void LoadMap(const wchar_t* fileName);
+void SaveRuleTree(const wchar_t* fileName);
+void LoadRuleTree(const wchar_t* fileName);
+void LoadLispCode(const wchar_t* fileName);
+
+void SaveMap(const char* fileName);
+void LoadMap(const char* fileName);
+void SaveRuleTree(const char* fileName);
+void LoadRuleTree(const char* fileName);
+void LoadLispCode(const char* fileName);
+void DeleteWholeMap();
 
 inline void UpdatePoints()
 {
 	if (!pSet.size())
 		return;
 	int i;
+	pointCount = 0;
 	auto itnext = pSet.begin();
 	++itnext;
 	for (auto it = pSet.begin(); it != pSet.end();)
@@ -53,6 +77,7 @@ inline void UpdatePoints()
 		it.second->state = it.second->newstate;
 		if (it.second->state)
 		{
+			pointCount++;
 			for (i = 0; i < numNeighbors; ++i)
 			{
 				if (it.second->neighbors[i] == NULL)
