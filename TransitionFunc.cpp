79a4473f7c98a25357bd90ea6fa2020dd3c5a2e3
ZZY <909645105@qq.com>
2015-06-12 12:15:20 +0800

    Update TransitionFunc.cpp

--- TransitionFunc.cpp
+++ TransitionFunc.cpp
@@ -16,16 +16,10 @@ _BYTE tmpByteArray[9];//[nw, ne, sw, se,] n, w, e, s, c
 short inverseNeighbors[8];
 int tmpDx[3][8] = { { 0 }, { 0, -1, 1, 0 }, { -1, 1, -1, 1, 0, -1, 1, 0 } };
 int tmpDy[3][8] = { { 0 }, { -1, 0, 0, 1 }, { -1, -1, 1, 1, -1, 0, 0, 1 } };
+int pointCount = 0;
 
-void FuncInit(char* src, int numstat, int numneigh)
+void NeighborsInit()
 {
-	if (sTree)
-		delete sTree;
-	pSet.clear();
-	sTree = LL_parser(src);
-	numStates = numstat;
-	num_nodes = 0;
-	numNeighbors = numneigh;
 	if (numNeighbors == 8)
 	{
 		inverseNeighbors[0] = 3; inverseNeighbors[1] = 2;
@@ -38,6 +32,20 @@ void FuncInit(char* src, int numstat, int numneigh)
 		inverseNeighbors[0] = 3; inverseNeighbors[1] = 2;
 		inverseNeighbors[2] = 1; inverseNeighbors[3] = 0;
 	}
+}
+
+void FuncInit(char* src, int numstat, int numneigh)
+{
+	if (sTree)
+		delete sTree;
+	pSet.clear();
+	pointCount = 0;
+	sTree = NULL;
+	sTree = LL_parser(src);
+	numStates = numstat;
+	num_nodes = 0;
+	numNeighbors = numneigh;
+	NeighborsInit();
 }
 
 int Function(int nw, int n, int ne, int w, int c, int e, int sw, int s, int se)
@@ -104,6 +112,7 @@ int dfs(int at)
 
 void GetRuleTree()
 {
+	nodeMap.clear();
 	numParams = numNeighbors + 1;
 	dfs(numParams);
 	printf("num_states=%d\n", numStates);
@@ -123,6 +132,8 @@ void AddPoint(int x, int y, _BYTE in)
 	auto tmpit = pSet.find(make_pair(x, y));
 	if (tmpit == pSet.end())
 	{
+		if (in)
+			pointCount++;
 		new Point(x, y, in);
 		if (in)
 		{
@@ -144,6 +155,8 @@ void AddPoint(int x, int y, _BYTE in)
 	{
 		if (in)
 		{
+			if (tmpit->second->state == 0)
+				pointCount++;
 			tmpit->second->state = in;
 			if (numNeighbors == 8)
 			{
@@ -164,6 +177,8 @@ void AddPoint(int x, int y, _BYTE in)
 void DeletePoint(int x, int y, _BYTE in)
 {
 	auto it = pSet.find(make_pair(x, y));
+	if (it->second->state)
+		pointCount--;
 	it->second->state = 0;
 }
 
@@ -203,7 +218,6 @@ Point::Point(int x, int y, _BYTE in)
 
 Point::~Point()
 {
-	pSet.erase(it);
 	for (short i = 0; i < numNeighbors; ++i)
 	{
 		if (neighbors[i])
@@ -223,5 +237,151 @@ void Point::Update()
 	tmpByteArray[numNeighbors] = state;
 	newstate = CheckRule(tmpByteArray);
 	if (newstate == -1)
+	{
+		pSet.erase(this->it);
 		delete this;
+	}
+}
+
+void DeleteWholeMap()
+{
+	for (auto &it : pSet)
+	{
+		delete it.second;
+	}
+	pSet.clear();
+}
+
+void SaveMap(const char* fileName)
+{
+	ofstream fout(fileName);
+	fout << GetPointCount() << endl;
+	for (auto &it : pSet)
+	{
+		if (it.second->state)
+			fout << it.first.first << " " << it.first.second << " " << (int)it.second->state << endl;
+	}
+	fout.close();
+}
+
+void LoadMap(const char* fileName)
+{
+	if (pSet.size())
+	{
+		DeleteWholeMap();
+	}
+	ifstream fin(fileName);
+	int len;
+	int tmpx, tmpy, tmpstate;
+	fin >> len;
+	for (int i = 0; i < len; ++i)
+	{
+		fin >> tmpx >> tmpy >> tmpstate;
+		AddPoint(tmpx,tmpy,tmpstate);
+	}
+	fin.close();
+}
+
+void SaveRuleTree(const char* fileName)
+{
+	ofstream fout(fileName);
+	if (!fout)
+		throw runtime_error("FILE ERROR");
+	fout << numStates<<endl;
+	fout << numNeighbors<<endl;
+	fout << num_nodes<<endl;
+	for (int i = 0; i < num_nodes; ++i)
+	{
+		for (int j = 0; j <= numStates; ++j)
+		{
+			fout << ruleTree[i][j]<<' ';
+		}
+		fout << endl;
+	} 
+	fout.close();
+}
+
+void LoadRuleTree(const char* fileName)
+{
+	ifstream fin(fileName);
+	if (!fin)
+		throw runtime_error("FILE ERROR");
+	fin >> numStates;
+	fin >> numNeighbors;
+	fin >> num_nodes;
+	for (int i = 0; i < num_nodes; ++i)
+	{
+		for (int j = 0; j <= numStates; ++j)
+		{
+			fin >> ruleTree[i][j];
+		}
+	}
+	NeighborsInit();
+	fin.close();
+}
+
+void LoadLispCode(const char* fileName)
+{
+	ifstream fin(fileName);
+	if (!fin)
+		throw runtime_error("File Faild");
+	int tmpnumStates, tmpnumNeighbors;
+	char line[2048];
+	fin.getline(line,2000);
+	sscanf_s(line,"%d%d",&tmpnumStates,&tmpnumNeighbors);
+	fin.getline(line, 2000);
+//	char lispCode[2048];
+//	sscanf_s(line,"%s",&lispCode[0]);
+	FuncInit(line, tmpnumStates, tmpnumNeighbors);
+	GetRuleTree();
+}
+
+void ConvertWcharToChar(char*& dst,const wchar_t* src)
+{
+	size_t len = wcslen(src);
+	dst = new char[len + 1];
+	for (size_t i = 0; i < len; ++i)
+	{
+		dst[i] = (char)src[i];
+	}
+}
+
+void SaveMap(const wchar_t* fileName)
+{
+	char* tmpStr = NULL;
+	ConvertWcharToChar(tmpStr,fileName);
+	SaveMap(tmpStr);
+	delete tmpStr;
+}
+
+void LoadMap(const wchar_t* fileName)
+{
+	char* tmpStr = NULL;
+	ConvertWcharToChar(tmpStr, fileName);
+	LoadMap(tmpStr);
+	delete tmpStr;
+}
+
+void SaveRuleTree(const wchar_t* fileName)
+{
+	char* tmpStr = NULL;
+	ConvertWcharToChar(tmpStr, fileName);
+	SaveRuleTree(tmpStr);
+	delete tmpStr;
+}
+
+void LoadRuleTree(const wchar_t* fileName)
+{
+	char* tmpStr = NULL;
+	ConvertWcharToChar(tmpStr, fileName);
+	LoadRuleTree(tmpStr);
+	delete tmpStr;
+}
+
+void LoadLispCode(const wchar_t* fileName)
+{
+	char* tmpStr = NULL;
+	ConvertWcharToChar(tmpStr, fileName);
+	LoadLispCode(tmpStr);
+	delete tmpStr;
 }
